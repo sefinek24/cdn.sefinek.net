05d3c4c3a77b63924179ce8dab58eb3f6800ef9e
Sefinek <sefinek5916@gmail.com>
2023-02-27 05:07:49 +0100

    Create ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
 		script          : './index.js',
 
 		log_date_format : 'HH:mm:ss.SSS DD.MM.YYYY',
-		error_file      : '/home/ubuntu/logs/www/cdn.sefinek.net/error.log',
-		out_file        : '/home/ubuntu/logs/www/cdn.sefinek.net/out.log',
+		error_file      : '/home/sefinek/logs/www/cdn.sefinek.net/error.log',
+		out_file        : '/home/sefinek/logs/www/cdn.sefinek.net/out.log',
 
 		max_restarts          : 8,
 		restart_delay         : 6000,
